1ac77a0a1879de7c63ebf4aaab009b0ea63a402b
m1t9 <dpleshanov@gmail.com>
2026-05-02 12:47:44 +0200

    task-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://n21negag10.execute-api.us-east-1.amazonaws.com/development",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
