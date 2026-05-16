25241e635c1154c2b8483701cc57682d11165f06
m1t9 <dpleshanov@gmail.com>
2026-05-16 13:16:28 +0200

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://n21negag10.execute-api.us-east-1.amazonaws.com/development",
+  product: "https://82g1vt6ce6.execute-api.us-east-1.amazonaws.com/development",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://zp6lrxx0ic.execute-api.us-east-1.amazonaws.com/development",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
