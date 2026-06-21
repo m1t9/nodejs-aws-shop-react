4b4ef9770727a47e8919e62749f2573892b46a45
m1t9 <dpleshanov@gmail.com>
2026-06-21 14:51:41 +0200

    task-9: updated cartApi  wit EB url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://82g1vt6ce6.execute-api.us-east-1.amazonaws.com/development",
-  order: "https://9iq2qtot7f.execute-api.us-east-1.amazonaws.com/prod/api",
+  order: "http://m1t9-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
   import: "https://zp6lrxx0ic.execute-api.us-east-1.amazonaws.com/development",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://9iq2qtot7f.execute-api.us-east-1.amazonaws.com/prod/api",
+  cart: "http://m1t9-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
