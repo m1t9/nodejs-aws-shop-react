7e9da0b21b7c90c42c0d2571c06868b625556c75
m1t9 <dpleshanov@gmail.com>
2026-06-21 20:21:31 +0200

    task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://82g1vt6ce6.execute-api.us-east-1.amazonaws.com/development",
-  order: "http://m1t9-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
+  order: "https://d13h3190oql71t.cloudfront.net/api",
   import: "https://zp6lrxx0ic.execute-api.us-east-1.amazonaws.com/development",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://m1t9-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
+  cart: "https://d13h3190oql71t.cloudfront.net/api",
 };
 
 export default API_PATHS;
